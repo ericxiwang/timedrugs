4ff56e355f164c81498e18bfd0494bf19378fca6
fredriksson <gowest.wang@gmail.com>
2015-06-21 00:05:22 -0700

    add promotion
    add some code into different files for promotion function

--- product_introduce.php
+++ product_introduce.php
@@ -27,20 +27,42 @@ include 'nav_menu_banner.php';
    
 <?php
 
-  //////////////////////////////
+  ////////////////////////////// to avoid dupicated submit //////////////
   $code = mt_rand(0,1000000);
   $_SESSION['code'] = $code;
 
 
 
   $pro_code = $_GET['id'];
-  $pro_query_main = "SELECT * FROM product_info WHERE pro_code ='$pro_code'";
+  $pro_query_main = "SELECT pro_name,pro_brand,pro_img,pro_description,pro_code,promotion_id,pro_o_price,promotion_enabled FROM product_info WHERE pro_code ='$pro_code'";
   $current_product = mysqli_query($db_connect,$pro_query_main);
   $current_product = mysqli_fetch_assoc($current_product);
+  $promotion_id_show = $current_product['promotion_id'];
+  /////////////// begin query if promotion is enabled /////////////
+  if ($current_product['promotion_enabled'] == '1'){
+    $current_promition_id = $current_product['promotion_id'];
+    $show_promotion_info = "SELECT promotion_id,pro_type,discount_value,pro_buy,pro_get,pro_price,dis_start_time,dis_end_time,dis_endless from pro_discount where promotion_id = '$promotion_id_show' and dis_enabled ='1'";
+    $show_promotion_info = mysqli_query($db_connect,$show_promotion_info);
+    $show_promotion_info = mysqli_fetch_assoc($show_promotion_info); 
+
+    $show_enable = 'yes';
+
+
+  }
+  else {
+
+    $show_enable = 'no';
+
+  }
+
+
+
+
 
   echo "<form action='shopping_cart_list.php' method='POST'>";
   //////////hidden element for session
   echo "<input type='hidden' name='originator' value='$code'>";
+  ////// ADD this line for submitting to next page ////////
   echo "<input name ='pro_code' type='hidden' value=$current_product[pro_code]>";    
 
 
@@ -51,17 +73,51 @@ include 'nav_menu_banner.php';
   echo "<img class='img-thumbnail preview-pic' src='$current_product[pro_img]'/></div>";
 
   echo "<div class='col-md-4 col-lg-4 panel panel-danger order-page'>";
-  echo "<ul class='list-group'>
-        <li class='list-group-item'>产品名称：$current_product[pro_name]</li>
-        <li class='list-group-item'>产品品牌：力达</li>
-        <li class='list-group-item'>净重：1公斤</li>
-        <li class='list-group-item'>邮寄重量：2公斤</li>
-        <li class='list-group-item'>规格：60粒</li>
-        <h4><li class='list-group-item'>零售价格：$current_product[pro_o_price]</li>
-        <li class='list-group-item'>特价价格：90</li></h4>
-        <li class='list-group-item'>净重：1公斤</li>
+  echo "<ul class='list-group'>";
+
+  echo "<li class='list-group-item '><h4>产品名称: ".$current_product['pro_name']."</h4></li>";
+  echo "<li class='list-group-item'>产品品牌: ".$current_product['pro_brand']."</li>";
+  echo "<li class='list-group-item'>规格：60粒</li>";
+  #echo "<h4><li class='list-group-item'>零售价格：".$current_product['pro_']"</li>";
+  ///////////in terms of promotion_id and promotion_type draw the promotion segement ///////
+  if ($show_enable == 'yes'){
+
+
+    switch ($show_promotion_info['pro_type'])
+    {
+      case '1'://discount
+        $show_discount = $show_promotion_info['discount_value'];
+        echo "<li class='list-group-item'>折扣:".$show_discount."％</li>";
+        echo "<li class='list-group-item'>零售价格(CAD):".$current_product['pro_o_price']."</li>";
+
+        echo "<input type='hidden' value='$current_promition_id' name='notice_promotion'>";
+
+
+        break;
+      case '2'://buy and get
+        $show_buy = $show_promotion_info['pro_buy'];
+        $show_get = $show_promotion_info['pro_get'];
+        echo "<li class='list-group-item'><h4 class='text-danger'>买 ".$show_buy." 赠 ".$show_get."</h4></li>";
+        echo "<li class='list-group-item'><h4>零售价格(CAD): "."$".$current_product['pro_o_price']."</h4></li>";
+
+        echo "<input type='hidden' value='$current_promition_id' name='notice_promotion'>";
+
+
+
+        break;
+      case '3'://speical price
+        $show_price = $show_promotion_info['pro_price'];
+        echo "<li class='list-group-item'><del class='text-danger'>零售价格(CAD): "."$".$current_product['pro_o_price']."</del></li>";
+        echo "<li class='list-group-item'><h4 class='text-success'>优惠价格(CAD): "."$".$show_price."</h4></li>";
+        echo "<input type='hidden' value='$current_promition_id' name='notice_promotion'>";
+        
+        break;
+    }
+
+  }
+
   
-      </ul>
+  echo "</ul>
 
       </div>";
   echo "<div class='col-md-5 col-lg-5 panel panel-danger order-page-m' >
--- shopping_cart_list.php
+++ shopping_cart_list.php
@@ -43,12 +43,72 @@ if (isset($_GET['product_id'])){
 else
 {	
 
+function check_promotion($current_id,$current_quantity,$current_price){
+	global $db_connect;
 
+
+
+		/////////////// go to check table pro_discount //////////////
+	$check_promotion_table = "SELECT discount_value,pro_buy,pro_get,pro_type,pro_price,dis_enabled from pro_discount where promotion_id ='$current_id' ";
+	$check_promotion_table = mysqli_query($db_connect,$check_promotion_table);
+	$check_promotion_table = mysqli_fetch_assoc($check_promotion_table);
+
+	if ($check_promotion_table['dis_enabled']=='1'){
+		print_r($check_promotion_table);
+
+		switch ($check_promotion_table['pro_type'])
+			{
+
+	      case '1'://discount
+	        $show_discount = $check_promotion_table['discount_value'];
+	        $promotion_result = intval($current_price * ($show_discount/100));
+	        $promotion_display = $current_price;
+	  
+
+	        return array($check_promotion_table['pro_type'],$promotion_result,$promotion_display);
+
+	        
+	        
+
+	        break;
+	      case '2'://buy and get
+	        $show_buy = $check_promotion_table['pro_buy'];
+	        $show_get = $check_promotion_table['pro_get'];
+	        $real_get = intval($current_quantity/$show_buy);
+
+	        $promotion_result = $current_quantity + $real_get*$show_get;
+	        $promotion_display = $current_quantity." + ".$real_get*$show_get;
+
+	        return array($check_promotion_table['pro_type'],$promotion_result,$promotion_display);
+
+
+	       
+
+
+
+	        break;
+	      case '3'://speical price
+	        $show_price = $check_promotion_table['pro_price'];
+	       
+	        
+	        break;
+	    }
+
+		
+
+	}
+
+	
+
+
+}
 if (isset($_POST['text_box'])){
-$pro_quantity = $_POST['text_box'];
-$pro_code = $_POST['pro_code'];
+$pro_quantity = $_POST['text_box']; // get order quantity without promotion
+$pro_code = $_POST['pro_code'];//get product code
 
-$current_product_query = ("SELECT pro_name,pro_img,pro_o_price,pro_code FROM product_info WHERE pro_code='$pro_code'");
+
+
+$current_product_query = ("SELECT pro_name,pro_img,pro_o_price,pro_code,promotion_id,promotion_enabled FROM product_info WHERE pro_code='$pro_code'");
 $add_to_cart = mysqli_query($db_connect,$current_product_query);
 $add_to_cart = mysqli_fetch_assoc($add_to_cart);
 
@@ -66,6 +126,17 @@ if(isset($_POST['originator'])) {
 
 				#echo "no session so create a new one";
 				$total_list = array();
+
+				/////////////////////////// invoke check promotion function //////////////////////
+				if (isset($_POST['notice_promotion'])){
+					echo "have promotion1";
+				$current_promotion = $_POST['notice_promotion'];//get matched promotion_id
+
+				check_promotion($current_promotion,$pro_quantity,$add_to_cart['pro_o_price']);
+				}
+				
+				
+
 				$add_to_cart['quantity'] = $pro_quantity;//insert quantity into session
 				$add_to_cart['whole_price'] = $add_to_cart['pro_o_price'] * $pro_quantity; //caculate whole price for one product and insert into session
 
@@ -86,6 +157,13 @@ if(isset($_POST['originator'])) {
 				#	echo "session exist, push one more into array";
 				// read previous data from session
 				$total_list = $_SESSION['cart_list'];
+
+
+				if (isset($_POST['notice_promotion'])){
+					echo "have promotion2";
+				$current_promotion = $_POST['notice_promotion'];//get matched promotion_id
+				check_promotion($current_promotion,$pro_quantity,$add_to_cart['pro_o_price']);
+				}
 
 			
 				$add_to_cart['quantity'] = $pro_quantity;
