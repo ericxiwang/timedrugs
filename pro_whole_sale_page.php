ceddb126d6664ecc70da5ead276a6df9117f04c0
fredriksson <gowest.wang@gmail.com>
2015-09-17 18:28:56 +0800

    change promotion

--- pro_whole_sale_page.php
+++ pro_whole_sale_page.php
@@ -16,10 +16,37 @@ include 'promotion_function.php';
       <!-- one inventory unit -->
 
      <?php
-     $query_all_pro  = "SELECT pro_name,pro_img,pro_o_price,pro_code,promotion_enabled,promotion_id from product_info where pro_onsell='1' and whole_sale='1'";
+
+
+
+
+     $query_buy_get = "SELECT pro_name,pro_img,pro_o_price,pro_code,promotion_enabled,promotion_id from product_info where promotion_enabled = '1' and whole_sale = '0'";
+     $query_buy_get = mysqli_query($db_connect,$query_buy_get);
+     show_product($query_buy_get);
+
+
+
+
+
+
+
+     $query_all_pro  = "SELECT pro_name,pro_img,pro_o_price,pro_code,promotion_enabled,promotion_id from product_info where pro_onsell='1' and whole_sale='1' and promotion_enabled = '1'";
      $all_products = mysqli_query($db_connect,$query_all_pro);
 
      show_product($all_products);
+
+
+
+
+
+
+
+
+
+
+
+
+
      
      ?>
     
